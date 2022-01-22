f4b4cfbff83002b83c7557e16facd60f3ce75a2c
hahahaha3 <eksql2689@gmail.com>
2022-01-22 23:42:49 +0900

    v1.36

--- js/youtube.js
+++ js/youtube.js
@@ -1,58 +1,41 @@
-/*
-'AIzaSyAMeYaiQ_kCtY_ZWYUq46p2gSQZowgTuSA'
-
-url : 'https://www.googleapis.com/youtube/v3/playlistItems'
-
-호출시 옵션값
-part: 'snippet',
-key:  'AIzaSyAMeYaiQ_kCtY_ZWYUq46p2gSQZowgTuSA',
-playlistId: 'PLjl-u2y72YNyCzhFSB91DD0oVvlE8mVYq',
-PLx62HH_9oB7hsMTmndaVmZPWoZNWoJBPb,
-maxResult: 5
-*/
-
-
 const vidList = document.querySelector('.vidList');
 const key = 'AIzaSyAMeYaiQ_kCtY_ZWYUq46p2gSQZowgTuSA';
-const playlistId = 'PLjl-u2y72YNyCzhFSB91DD0oVvlE8mVYq';
-const num = 5;
-const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlistId}&maxResult=${num}`; ﻿
-
+const playlistId = 'PLx62HH_9oB7gzkqn1mQQlLKcyMDYlWXPe';
+const num = 12;
+const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlistId}&maxResults=${num}`;
 
 fetch(url)
 .then(data => {
     return data.json();
 })
 .then(json => {
-    // console.log(json);
     let items = json.items;
     let result = '';
     items.map(item => {
-
         let title = item.snippet.title;
-        if(title.length > 30) {
-            title = title.substr(0, 30) + '...';
+        if(title.length > 50) {
+            title = title.substr(0, 50) + '...';
         }
 
         let con = item.snippet.description;
-        if(con.length > 200) {
-            con = title.substr(0, 200) + '...';
+        if(con.length > 150) {
+            con = title.substr(0, 150) + '...';
         }
 
         let date = item.snippet.publishedAt;
         date = date.split('T')[0];
 
         result += `
-                        <article>
-                            <a href="${item.snippet.resourceId.videoId}" class="pic">
-                                <img src="${item.snippet.thumbnails.medium.url}" alt="">
-                            </a>
-                            <div class="con">
-                                <h2>${title}</h2>
-                                <p>${con}</p>
-                                <span>${date}</span>
-                            </div>
-                        </article>
+            <article>
+                <a href="${item.snippet.resourceId.videoId}" class="pic">
+                    <img src="${item.snippet.thumbnails.medium.url}" alt="">
+                </a>
+                <div class="con">
+                    <h2>${title}</h2>
+                    <p>${con}</p>
+                    <span>${date}</span>
+                </div>
+            </article>
         `;
     });
     vidList.innerHTML = result;
@@ -61,16 +44,14 @@ fetch(url)
 vidList.addEventListener('click', e => {
     e.preventDefault();
 
-    // a요소의 href값을 받아야 하므로
-    // 클릭한 요소의 부모태그가 a요소가 아니라면 중지
     if(!e.target.closest('a')) return;
 
     const vidId = e.target.closest('a').getAttribute('href');
     let pop = document.createElement('figure');
     pop.classList.add('pop');
     pop.innerHTML = `
-                                <iframe src="https://www.youtube.com/embed/${vidId}" frameborder="0" width="100%" height="100%" allowfullscreen></iframe>
-                                <span class="btnClose">Close</span>
+            <iframe src="https://www.youtube.com/embed/${vidId}" frameborder="0" width="100%" height="100%" allowfullscreen></iframe>
+            <span class="btnClose">Close</span>
     `;
     vidList.append(pop);
 });
@@ -85,4 +66,3 @@ vidList.addEventListener('click', e => {
         }
     }
 })
-
