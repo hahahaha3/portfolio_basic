b685d9458af71eb393ad8fee552024669a0cb455
hahahaha3 <eksql2689@gmail.com>
2022-01-25 03:13:53 +0900

    v1.5

---
+++ js/about.js
@@ -0,0 +1,29 @@
+const frame = document.querySelector('.profile');
+fetch('data/members.json')
+    .then(data => {
+        return data.json();
+    })
+    .then(json => {
+        const memberInfo = json.data;
+        let tags = '';
+        memberInfo.map(member => {
+            tags+= `
+            <article class="member_article">
+                <div class="line">
+                    <img src="${member.pic}">
+                    <div class="txtBox">
+                        <h2>${member.name}</h2>
+                        <span>${member.position}</span>
+                        <p>${member.comment}</p>
+                        <div class="icons">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fas fas fa-envelope"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        `;
+        })
+        frame.innerHTML = tags;
+    })
--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
-// defer 덕에 사용 안해도 됨
-// window.onload = function() {
-//     // 실행코드
-// }
+class BtnCall {
+    constructor() {
+        this.init();
+        this.bindingEvent();
+    }
+    init() {
+        this.btnCall = document.querySelector(".btnCall");
+        this.menuMo = document.querySelector(".menuMo");
+    }
 
-// 변수 설정
-const btnCall = document.querySelector(".btnCall");
-const menuMo = document.querySelector(".menuMo");
-
-// 이벤트 바인딩(이벤트연결)
-// btnCall을 클릭할 때 
-btnCall.onclick = function(e) {
-    // 링크이동금지
-    e.preventDefault(); 
-
-    // btnCall에 on이 있으면 제거, 없으면 추가
-    btnCall.classList.toggle("on");
-    // menuMo에 on이 있으면 제거, 없으면 추가
-    menuMo.classList.toggle("on");
+    bindingEvent() {
+        this.btnCall.addEventListener('click', e => {
+            e.preventDefault(); 
+            this.btnCall.classList.toggle("on");
+            this.menuMo.classList.toggle("on");
+        });
+    }
 }
-
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
